a147695e21896809b93bed498d70049767c01199
Briana Garcia <briana_garcia_@outlook.com>
2025-11-07 21:37:23 -0800

    Edit Task Screen Progress

--- Scottys-Schedule/app/editTask.jsx
+++ Scottys-Schedule/app/editTask.jsx
@@ -1,14 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { View } from 'react-native'
+import EditTaskForm from '../components/tasks/editTaskForm'
 
 const EditTask = () => {
   return (
     <View>
-      <Text>EditTask</Text>
+      <EditTaskForm/>
     </View>
   )
 }
 
 export default EditTask
-
-const styles = StyleSheet.create({})
